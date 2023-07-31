be63bb22e8702177f3d3684de25de1108ef2f2f1
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2023-07-31 18:50:48 +0300

    _v1g

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,6 @@
-export default function Card({ masterName, masterPhoto }) {
+import { MasterCardPropsTS } from './types';
+
+export default function Card({ masterName, masterPhoto }: MasterCardPropsTS) {
   return (
     <div className="m-w-[300px] min-w-[140px] bg-gray-50 border border-solid border-red-400 flex flex-col items-center justify-center gap-8">
       <img src={masterPhoto} width={60} height={60} alt={masterName} />
---
+++ src/components/types.ts
@@ -0,0 +1,4 @@
+export interface MasterCardPropsTS {
+  masterName: string;
+  masterPhoto: string;
+}
