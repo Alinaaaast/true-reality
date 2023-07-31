8f7bde7c6e40effe37d49ed2a82ebffb87f48f88
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2023-07-31 18:45:52 +0300

    _v1f

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Navigation from './layout/Navigation';
 import Header from './layout/Header';
-import Masters from './layout/Masters';
+// import Masters from './layout/Masters';
 import Schedule from './layout/Schedule';
 import Footer from './layout/Footer';
 import Advantages from './layout/Advantages';
