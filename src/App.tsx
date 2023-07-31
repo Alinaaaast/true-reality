4193d1c8711a155b13fc3ba5f2c6ed1c02e005f2
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2023-07-31 18:43:34 +0300

    _v1s

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       <Navigation />
       <Header />
       <Advantages />
-      <Masters />
+      {/* <Masters /> */}
       <Schedule />
       <Gallery />
       <Footer />
