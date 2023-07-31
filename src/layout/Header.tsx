d2f55b15eb9169b7a9a0e60da77c3b9b27344559
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2023-07-31 18:57:13 +0300

    _v1h

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <>
       <div className="flex h-screen justify-center items-center flex-col">
-        <div className="w-full h-screen bg-[url('images/hero_image.jpg')] bg-cover bg-center ">
+        <div className="w-full h-screen bg-[url('../images/hero_image.jpg')] bg-cover bg-center ">
           <div className="w-full h-full flex  flex-col justify-center items-center backdrop-brightness-50">
             <span className="text-white text-4xl w-1/2 text-center">
               True Reality Festival
