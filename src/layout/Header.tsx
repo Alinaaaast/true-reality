6a89d7f4bfda92859d6da4778964402d8fb5f85d
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2023-07-31 18:44:37 +0300

    _v1d

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,10 +1,10 @@
 export default function Header() {
   return (
     <>
-      <div class="flex h-screen justify-center items-center flex-col">
-        <div class="w-full h-screen bg-[url('images/hero_image.jpg')] bg-cover bg-center ">
-          <div class="w-full h-full flex  flex-col justify-center items-center backdrop-brightness-50">
-            <span class="text-white text-4xl w-1/2 text-center">
+      <div className="flex h-screen justify-center items-center flex-col">
+        <div className="w-full h-screen bg-[url('images/hero_image.jpg')] bg-cover bg-center ">
+          <div className="w-full h-full flex  flex-col justify-center items-center backdrop-brightness-50">
+            <span className="text-white text-4xl w-1/2 text-center">
               True Reality Festival
             </span>
             <p className="text-white mt-4">
--- src/layout/Navigation.tsx
+++ src/layout/Navigation.tsx
@@ -1,6 +1,5 @@
 import logo from '../../public/images/tr_logo.png';
 import { useState } from 'react';
-import Link from 'next/link';
 
 export default function Navigation() {
   const [isOpen, setIsOpen] = useState(false);
