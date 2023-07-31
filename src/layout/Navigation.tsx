287f2d4a61214c7aefa0040d998cd5f959a00052
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2023-07-31 19:16:51 +0300

    _v1h

--- src/layout/Navigation.tsx
+++ src/layout/Navigation.tsx
@@ -5,7 +5,7 @@ export default function Navigation() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="flex items-center justify-center  sticky top-0 bg-white z-10">
-      <nav className="flex items-center justify-between flex-wrap p-4 max-w-7xl ">
+      <nav className="flex items-center justify-between flex-wrap p-4 max-w-7xl min-w-[260px] ">
         <div className="flex items-center flex-shrink-0 text-white mr-6 lg:mr-72">
           <a href="/">
             <img
@@ -21,14 +21,14 @@ export default function Navigation() {
             className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400"
           >
             <svg
-              className={`fill-current h-3 w-3 ${isOpen ? 'hidden' : 'block'}`}
+              className={`fill-current h-6 w-6 ${isOpen ? 'hidden' : 'block'}`}
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
             </svg>
             <svg
-              className={`fill-current h-3 w-3 ${isOpen ? 'block' : 'hidden'}`}
+              className={`fill-current h-6 w-6 ${isOpen ? 'block' : 'hidden'}`}
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
@@ -41,7 +41,7 @@ export default function Navigation() {
             isOpen ? 'block' : 'hidden'
           }`}
         >
-          <div className="text-sm lg:flex-grow flex items-end justify-end">
+          <div className="text-base lg:flex-grow flex flex-col lg:flex-row items-center justify-center">
             <a
               href="/#masters"
               className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4"
