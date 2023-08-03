b24d9a60b73dce716a06e3a445af5fb958153e9a
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2023-08-03 12:59:00 +0300

    _v1t

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
         <div className="w-full h-screen bg-[url('../images/header_bg_v.jpeg')] md:bg-[url('../images/header_bg_h.jpeg')] bg-cover bg-center ">
           <div className="w-full h-full flex flex-col justify-center items-center backdrop-brightness-20 ">
             <div className="bg-[#372821]/70 w-[100%] flex items-center justify-center flex-col py-8">
-              <span className="text-white text-4xl md:text-6xl w-1/2 text-center uppercase">
+              <span className="text-white text-3xl md:text-6xl w-3/4 text-center uppercase font-bold">
                 True Reality Festival
               </span>
               {/* <p className="text-white text-center mt-8">
@@ -13,7 +13,7 @@ export default function Header() {
                 13.08
                 <br></br> SKVOspace, Київ
               </p> */}
-              <p className="text-white mt-8 text-lg md:text-2xl max-w-xl text-center px-8">
+              <p className="text-white mt-8 text-md md:text-2xl max-w-xl text-center px-8">
                 Місце, де всі свої, де всі такі різні, але чимось дуже схожі на
                 тебе. Місце, де ти почуваєш себе вільно.
               </p>
