1376d27cfab8d2e48a4238baf6468c4235688a48
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2023-08-03 19:57:02 +0300

    _v1i

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Footer from './layout/Footer';
 import Advantages from './layout/Advantages';
 import Gallery from './layout/Galley';
 import Place from './layout/Place';
+import Price from './layout/Price';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Masters />
       <Schedule />
       <Gallery />
+      <Price />
       <Place />
       <Footer />
     </>
---
+++ src/layout/Price.tsx
@@ -0,0 +1,63 @@
+export default function Price() {
+  return (
+    <>
+      <div className="bg-gray-100 py-16">
+        <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="text-center">
+            <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
+              Ціна
+            </h2>
+          </div>
+          <div className="mt-10">
+            <div className="max-w-[400px] md:max-w-4xl mx-auto grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+              <div className="bg-white overflow-hidden shadow rounded-lg">
+                <div className="px-4 py-5 sm:p-6 flex flex-col items-center justify-center ">
+                  <h3 className="text-2xl leading-6 font-medium text-gray-900">
+                    Early birds
+                  </h3>
+                  <p className="mt-4 text-md text-gray-500">до 5.08</p>
+                  <div className="mt-6">
+                    <div className="text-3xl font-semibold text-gray-900">
+                      800 грн
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="bg-white overflow-hidden shadow rounded-lg">
+                <div className="px-4 py-5 sm:p-6 flex flex-col items-center justify-center">
+                  <h3 className="text-2xl leading-6 font-medium text-gray-900">
+                    Золота середина
+                  </h3>
+                  <p className="mt-4 text-md text-gray-500">3 6.08 по 12.08</p>
+                  <div className="mt-6">
+                    <div className="text-3xl font-semibold text-gray-900">
+                      1000 грн
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="bg-white overflow-hidden shadow rounded-lg">
+                <div className="px-4 py-5 sm:p-6 flex flex-col items-center justify-center">
+                  <h3 className="text-2xl leading-6 font-medium text-gray-900">
+                    В день заходу
+                  </h3>
+                  <p className="mt-4 text-md text-gray-500">13.08</p>
+                  <div className="mt-6">
+                    <div className="text-3xl font-semibold text-gray-900">
+                      1 200 грн
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              {/* Plan 2 and Plan 3 */}
+              {/* Add similar code for additional plans */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
