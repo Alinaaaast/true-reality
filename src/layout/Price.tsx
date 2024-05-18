bd40ce9d71e11a0ce795078e3219eb16dc74576e
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2024-05-18 15:28:43 +0300

    _v1y

--- src/layout/Price.tsx
+++ src/layout/Price.tsx
@@ -53,7 +53,7 @@ export default function Price() {
               </div>
 
               <a
-                href="#"
+                href="https://www.instagram.com/tr__fest"
                 class="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-orange-600 dark:hover:bg-orange-700 focus:outline-none dark:focus:ring-orange-800"
               >
                 Зареєструватись
