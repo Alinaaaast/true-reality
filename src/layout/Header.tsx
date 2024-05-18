a56b6ed39bb2c9742b0ca8b2c6fdad138d8f2eb4
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2024-05-18 15:50:41 +0300

    _v1p

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -16,6 +16,14 @@ export default function Header() {
                 Місце, де всі свої, де всі такі різні, але чимось дуже схожі на
                 тебе. Місце, де ти почуваєш себе вільно.
               </p>
+              <div className=" flex items-center justify-center">
+                <a
+                  href="https://www.instagram.com/tr__fest"
+                  className="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 mt-8"
+                >
+                  Зареєструватись
+                </a>
+              </div>
             </div>
           </div>
         </div>
--- src/layout/Price.tsx
+++ src/layout/Price.tsx
@@ -58,7 +58,7 @@ export default function Price() {
             <div className=" flex items-center justify-center">
               <a
                 href="https://www.instagram.com/tr__fest"
-                className="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+                className="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 mt-8"
               >
                 Зареєструватись
               </a>
